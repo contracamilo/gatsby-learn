0bae96e31488ba75651af9553da348b3ccffd588
Camilo Rivera <camilo.riveraq@gmail.com>
2019-09-15 22:07:56 -0500

    9.creating view with react

---
+++ src/components/jumbo.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { StyledJumbo } from "../styles/components"
+
+const Jumbo = () => {
+  return (
+    <StyledJumbo>
+      <div>
+        <h2>!Vendo Cositas!</h2>
+        <small>para que te mecatees la platica</small>
+      </div>
+    </StyledJumbo>
+  )
+}
+
+export default Jumbo
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import { Jumbo } from "../components"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
@@ -8,6 +8,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <Jumbo />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
